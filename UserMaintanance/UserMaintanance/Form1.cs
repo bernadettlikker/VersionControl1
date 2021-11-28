af721c24e14029f60cb20808001c44849e2bc982
bernadettlikker <bernadett.likker@stud.uni-corvinus.hu>
2021-11-28 21:46:26 +0100

    torles mukodo

--- UserMaintanance/UserMaintanance/Form1.cs
+++ UserMaintanance/UserMaintanance/Form1.cs
@@ -23,6 +23,7 @@ namespace UserMaintanance
             btnAdd.Text = Resource.Add;
             lblFullName.Text = Resource.FullName;
             btnFajlba.Text = Resource.Fajlbairas;
+            btnDelete.Text = Resource.Delete;
 
             listUsers.DataSource = users;
             listUsers.ValueMember = "ID";
@@ -57,33 +58,9 @@ namespace UserMaintanance
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            int valasztott = Convert.ToInt32(listUsers.SelectedValue);
-            User kivalasztott = (from i in context.Users
-                                 where i.ID == valasztott
-                                 select i).FirstOrDeafult();
-            context.Users.Remove(kivalasztott);
-            
-
-
-            int lID = Convert.ToInt32(listUsers.SelectedValue);
-            var od = from x in context.users
-                     where x.ID == lID
-                     select x;
-            users.Remove(od.FirstOrDefault());
-            
-
-
-            listUsers.Items.Remove(listUsers.SelectedItem);
-            ListBox.selectedItems = new ListBox.listUsers();
-            selectedItems = listUsers.SelectedItems;
-
-            if (listUsers.SelectedIndex != -1)
-            {
-                for (int i = selectedItems.Count - 1; i >= 0; i--)
-                    listUsers.Items.Remove(selectedItems[i]);
-            }
-            else
-                MessageBox.Show("Kérem jelölje ki melyik sort szeretné törölni");
+            User kivalasztott = (User)listUsers.SelectedItem;
+            users.Remove(kivalasztott);
+                        
         }
     }
 }
