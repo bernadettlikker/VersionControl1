a49c8ea893328dcb1d7a89a348f31311708b4b57
bernadettlikker <bernadett.likker@stud.uni-corvinus.hu>
2021-12-01 22:58:37 +0100

    ertek szamitas, var szamitas

--- VAR/VAR/Form1.cs
+++ VAR/VAR/Form1.cs
@@ -24,7 +24,27 @@ namespace VAR
 
             CreatePortfolio();
 
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
         }
+
+
 
         private void CreatePortfolio()
         {
@@ -38,6 +58,21 @@ namespace VAR
 
             dataGridView2.DataSource = Portfolio;
 
+        }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
 
         private void Form1_Load(object sender, EventArgs e)
